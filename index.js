a452070abfba1c288783328be910353d826eba92
Piet Geursen <pietgeursen@gmail.com>
2016-12-04 00:01:22 +1300

    Make editing rows work.

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ var h = require('@mmckegg/mutant/html-element');
 var Struct = require('@mmckegg/mutant/struct');
 var computed = require('@mmckegg/mutant/computed');
 var MutantArray = require('@mmckegg/mutant/array');
+var MutantMap = require('@mmckegg/mutant/map');
+var Value = require('@mmckegg/mutant/value');
+var when = require('@mmckegg/mutant/when');
 
 var main = document.querySelector('main')
 
 var state = Struct({ 
   title: 'Counter MUTANT',
   count: 0,
-  lineItems: MutantArray([{
+  lineItems: MutantArray([Struct({
     description: 'teaching work',
     qty: 10,
     price: 3,
     isEditing: false
-  },{
+  }),Struct({
     description: 'being kewl',
     qty: 2,
     price: 3,
     isEditing: true
-  }])
+  })])
 })
+
 
 const subtotal = computed([state.lineItems], items => items.reduce((sum, item) => { return sum + item.qty * item.price}, 0))
 const lineTotal = (item) => computed([item], item => item.qty * item.price)
@@ -30,17 +34,10 @@ console.log(subtotal())
 console.log(line1Total())
 line1Total(console.log)
 subtotal(console.log)
-
+state(console.log)
 
 function mutator(state, action) {
   switch (action.type) {
-    case 'SET_TITLE':
-      state.title.set(action.payload)
-      break;
-    case 'INCREMENT_COUNT':
-      state.count.set(state.count() + 1)
-      break;
-    default:
   }
 }
 
@@ -50,25 +47,29 @@ function dispatch(action) {
 
 function renderEditingItem(item){
   return  h('div.item', {}, [
-    h('input', {value: item.description}),
-    h('input', {value: item.qty}),
-    h('input', {value: item.price}),
+    h('input', {value: item.description, 'ev-change': (e) =>item.description.set(e.target.value)}),
+    h('input', {value: item.qty, 'ev-change': (e) =>item.qty.set(e.target.value)}),
+    h('input', {value: item.price, 'ev-change': (e) =>item.price.set(e.target.value)}),
     h('div', {}, lineTotal(item)),
+    h('button', {'ev-click': () => item.isEditing.set(false)}, 'Done')
   ])
 }
+
 function renderItem(item){
-  return  h('div.item', {}, [
+  return  h('div.item', {'ev-click': () => item.isEditing.set(true)}, [
     h('div', {}, item.description),
     h('div', {}, item.qty),
     h('div', {}, item.price),
     h('div', {}, lineTotal(item)),
   ])
 }
+
+
 function render (state, dispatch) {
   return h('div',{}, [
     h('h1', {}, state.title),
-    h('div#lines', {}, state.lineItems().map(item => {
-      return item.isEditing ? renderEditingItem(item) : renderItem(item) 
+    h('div#lines', {}, MutantMap(state.lineItems, item => {
+      return when(item.isEditing, renderEditingItem(item) , renderItem(item))
     }) 
     )
   ])
